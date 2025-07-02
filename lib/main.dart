e038b95beab3737283b5434acdc26d413d896120
khaled <khaledbahnawy@gmail.com>
2025-07-02 19:31:48 +0300

    call our service locator in main fun

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -20,7 +22,9 @@ class BooklyApp extends StatelessWidget {
         useMaterial3: false,
       ).copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        textTheme: GoogleFonts.montserratTextTheme(
+          ThemeData.dark().textTheme,
+        ),
       ),
     );
   }
