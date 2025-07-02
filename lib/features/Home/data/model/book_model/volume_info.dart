515155315239111be716f6a452f3948abfcd6ec9
khaled <khaledbahnawy@gmail.com>
2025-07-03 01:21:56 +0300

    integrate featured books cubit with featured list view and handdle casting exeptions

--- lib/features/Home/data/model/book_model/volume_info.dart
+++ lib/features/Home/data/model/book_model/volume_info.dart
@@ -48,43 +48,51 @@ class VolumeInfo extends Equatable {
     this.canonicalVolumeLink,
   });
 
-  factory VolumeInfo.fromJson(Map<String, dynamic> json) => VolumeInfo(
-    title: json['title'] as String?,
-    authors: json['authors'] as List<String>?,
-    publisher: json['publisher'] as String?,
-    publishedDate: json['publishedDate'] as String?,
-    description: json['description'] as String?,
-    industryIdentifiers:
-        (json['industryIdentifiers'] as List<dynamic>?)
-            ?.map((e) => IndustryIdentifier.fromJson(e as Map<String, dynamic>))
-            .toList(),
-    readingModes:
-        json['readingModes'] == null
-            ? null
-            : ReadingModes.fromJson(
-              json['readingModes'] as Map<String, dynamic>,
-            ),
-    pageCount: json['pageCount'] as int?,
-    printType: json['printType'] as String?,
-    categories: json['categories'] as List<String>?,
-    maturityRating: json['maturityRating'] as String?,
-    allowAnonLogging: json['allowAnonLogging'] as bool?,
-    contentVersion: json['contentVersion'] as String?,
-    panelizationSummary:
-        json['panelizationSummary'] == null
-            ? null
-            : PanelizationSummary.fromJson(
-              json['panelizationSummary'] as Map<String, dynamic>,
-            ),
-    imageLinks:
-        json['imageLinks'] == null
-            ? null
-            : ImageLinks.fromJson(json['imageLinks'] as Map<String, dynamic>),
-    language: json['language'] as String?,
-    previewLink: json['previewLink'] as String?,
-    infoLink: json['infoLink'] as String?,
-    canonicalVolumeLink: json['canonicalVolumeLink'] as String?,
-  );
+  factory VolumeInfo.fromJson(Map<String, dynamic> json) =>
+      VolumeInfo(
+        title: json['title'] as String?,
+        authors: (json['authors'] as List<dynamic>?)?.cast<String>(),
+        publisher: json['publisher'] as String?,
+        publishedDate: json['publishedDate'] as String?,
+        description: json['description'] as String?,
+        industryIdentifiers:
+            (json['industryIdentifiers'] as List<dynamic>?)
+                ?.map(
+                  (e) => IndustryIdentifier.fromJson(
+                    e as Map<String, dynamic>,
+                  ),
+                )
+                .toList(),
+        readingModes:
+            json['readingModes'] == null
+                ? null
+                : ReadingModes.fromJson(
+                  json['readingModes'] as Map<String, dynamic>,
+                ),
+        pageCount: json['pageCount'] as int?,
+        printType: json['printType'] as String?,
+        categories:
+            (json['categories'] as List<dynamic>?)?.cast<String>(),
+        maturityRating: json['maturityRating'] as String?,
+        allowAnonLogging: json['allowAnonLogging'] as bool?,
+        contentVersion: json['contentVersion'] as String?,
+        panelizationSummary:
+            json['panelizationSummary'] == null
+                ? null
+                : PanelizationSummary.fromJson(
+                  json['panelizationSummary'] as Map<String, dynamic>,
+                ),
+        imageLinks:
+            json['imageLinks'] == null
+                ? null
+                : ImageLinks.fromJson(
+                  json['imageLinks'] as Map<String, dynamic>,
+                ),
+        language: json['language'] as String?,
+        previewLink: json['previewLink'] as String?,
+        infoLink: json['infoLink'] as String?,
+        canonicalVolumeLink: json['canonicalVolumeLink'] as String?,
+      );
 
   Map<String, dynamic> toJson() => {
     'title': title,
@@ -92,7 +100,8 @@ class VolumeInfo extends Equatable {
     'publisher': publisher,
     'publishedDate': publishedDate,
     'description': description,
-    'industryIdentifiers': industryIdentifiers?.map((e) => e.toJson()).toList(),
+    'industryIdentifiers':
+        industryIdentifiers?.map((e) => e.toJson()).toList(),
     'readingModes': readingModes?.toJson(),
     'pageCount': pageCount,
     'printType': printType,
--- lib/features/Home/presentation/view/widgets/featured_list_view.dart
+++ lib/features/Home/presentation/view/widgets/featured_list_view.dart
@@ -1,23 +1,38 @@
+import 'package:bookly_app/core/widgets/custom_error_message.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/Home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/Home/presentation/manager/featured_books_cubit/featured_books_state.dart';
 import 'package:bookly_app/features/Home/presentation/view/widgets/custom_book_image_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedListView extends StatelessWidget {
   const FeaturedListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: CustomBookImageItem(),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: CustomBookImageItem(),
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorMessage(errMessage: state.errMessage);
+        } else {
+          return CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
