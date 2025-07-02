4e8aa05180a4f8a49660070f25545ca7475aa5c5
khaled <khaledbahnawy@gmail.com>
2025-07-02 17:25:20 +0300

    create featch Featured books in Featured Books Cubit

--- lib/features/Home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/Home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
@@ -1,6 +1,21 @@
+import 'package:bookly_app/features/Home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/Home/presentation/view_model/featured_books_cubit/featured_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit(super.initialState);
+  FeaturedBooksCubit(this.homeRepoImpl)
+    : super(FeaturedBooksInitial());
+  final HomeRepoImpl homeRepoImpl;
+  Future<void> fetchFeaturdBooks() async {
+    emit(FeaturedBooksLoading());
+    var books = await homeRepoImpl.fetchFeaturedBooks();
+    books.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(errMessage: failure.errMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books: books));
+      },
+    );
+  }
 }
--- lib/features/Home/presentation/view_model/featured_books_cubit/featured_books_state.dart
+++ lib/features/Home/presentation/view_model/featured_books_cubit/featured_books_state.dart
@@ -1,9 +1,19 @@
+import 'package:bookly_app/features/Home/data/model/book_model/book_model.dart';
+
 class FeaturedBooksState {}
 
 class FeaturedBooksInitial extends FeaturedBooksState {}
 
-class FeaturedBooksSuccess extends FeaturedBooksState {}
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
 
-class FeaturedBooksFailure extends FeaturedBooksState {}
+  FeaturedBooksSuccess({required this.books});
+}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  FeaturedBooksFailure({required this.errMessage});
+}
 
 class FeaturedBooksLoading extends FeaturedBooksState {}
